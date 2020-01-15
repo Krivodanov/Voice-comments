9a91816f02d20c431bf8694425bf01a05893e56d
Криводанов Евгений <pochta.evgen@gmail.com>
2020-01-15 12:43:07 +0600

    JS вынесен в отдельный файл.

---
+++ voice.js
@@ -0,0 +1,54 @@
+const URL = 'voice.php';
+let div = document.createElement('div');
+div.id = 'messages';
+let start = document.createElement('button');
+start.id = 'start';
+start.innerHTML = 'Start';
+let stop = document.createElement('button');
+stop.id = 'stop';
+stop.innerHTML = 'Stop';
+document.body.appendChild(div);
+document.body.appendChild(start);
+document.body.appendChild(stop);
+navigator.mediaDevices.getUserMedia({ audio: true})
+    .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+
+        document.querySelector('#start').addEventListener('click', function(){
+            mediaRecorder.start();
+        });
+        let audioChunks = [];
+        mediaRecorder.addEventListener("dataavailable",function(event) {
+            audioChunks.push(event.data);
+        });
+
+        document.querySelector('#stop').addEventListener('click', function(){
+            mediaRecorder.stop();
+        });
+
+        mediaRecorder.addEventListener("stop", function() {
+            const audioBlob = new Blob(audioChunks, {
+                type: 'audio/wav'
+            });
+
+            let fd = new FormData();
+            fd.append('voice', audioBlob);
+            sendVoice(fd);
+            audioChunks = [];
+        });
+    });
+
+async function sendVoice(form) {
+    let promise = await fetch(URL, {
+        method: 'POST',
+        body: form});
+    if (promise.ok) {
+        let response =  await promise.json();
+        console.log(response.data);
+        let audio = document.createElement('audio');
+        audio.src = response.data;
+        audio.controls = true;
+        audio.autoplay = true;
+        document.querySelector('#messages').appendChild(audio);
+    }
+}
